50e80645521863d81f21b82f4145a5ad335ab16e
sophiasussa <sophiasussa@outlook.com>
2025-12-02 16:35:36 -0300

    feat: add login service

---
+++ lib/services/login_service.dart
@@ -0,0 +1,23 @@
+import 'package:buskei/models/user.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class LoginService {
+  final String baseUrl = "";
+
+  Future<UserLoginResponse?> login(String email, String senha) async{
+    final url = Uri.parse("$baseUrl/login");
+
+    final response = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"email": email, "senha": senha}),
+    );
+
+    if (response.statusCode == 200){
+      return UserLoginResponse.fromJson(jsonDecode(response.body));
+    }
+
+    return null;
+  } 
+}
