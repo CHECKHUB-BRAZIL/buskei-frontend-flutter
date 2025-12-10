e9f791e8ac43b36c37710ea742fc8e4476110c6a
sophiasussa <sophiasussa@outlook.com>
2025-12-10 17:27:48 -0300

    feat: add auth_repository - domain

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,6 +1,14 @@
 import '../entities/user_entity.dart';
 
 abstract class AuthRepository {
-  Future<UserEntity> login(String email, String senha);
-  Future<UserEntity> register(String email, String senha);
+  Future<UserEntity> login({
+    required String email,
+    required String senha,
+  });
+
+  Future<UserEntity> register({
+    required String nome,
+    required String email,
+    required String senha,
+  });
 }
